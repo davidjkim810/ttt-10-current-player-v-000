632a9d637de57ca13432910d0ee6db97de8e73cb
David Kim <davidjkim810@gmail.com>
2017-12-13 20:04:00 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,6 @@
 def turn_count(board)
   turn = 0
-  if board.each {board.each == "X" || board.each == "O"
-   turn += 1}
+  if board.each {board.each == "X" || board.each == "O"}
+   turn += 1
+ end
 end
